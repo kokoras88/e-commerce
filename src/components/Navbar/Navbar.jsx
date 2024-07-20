45bec9ada4d22fdd1390754c479fad7f2d468e2a
Alexandros Georgiadis <kokoras19888@gmail.com>
2024-07-20 16:33:37 +0200

    e-commerce

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import { assets }  from '../../assets/assets'
+import { Link } from 'react-router-dom'
+
+
+const Navbar = ({setShowLogin}) => {
+
+  const [menu, setMenu] = useState('home')
+
+  return (
+    <div className='navbar'>
+
+
+              <Link to='/'><img src={assets.logo} alt="" className='logo' /></Link>
+
+              <ul className="navbar-menu">
+                <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</Link>
+                <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
+                <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</a>
+                <a href='#footer' onClick={()=>setMenu("contuct-us")} className={menu==="contuct-us"?"active":""}>contuct-us</a>
+              </ul>
+
+              <img src={assets.search_icon} alt="" />
+
+              <div className="navbar-search-icon">
+                    <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
+                    <div className="dot"></div>
+              </div>
+
+              <button onClick={()=>setShowLogin(true)}>sing in</button>
+
+
+    </div>
+
+  )
+}
+
+export default Navbar
